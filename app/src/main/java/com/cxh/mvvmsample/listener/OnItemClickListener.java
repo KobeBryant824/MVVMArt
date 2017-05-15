39887b6b4d65d53cde279fc15ed8ac801bcce402
cxh <haigod7@gmail.com>
2017-05-15 10:18:24 +0800

    :see_no_evil:

---
+++ app/src/main/java/com/cxh/mvvmsample/listener/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.cxh.mvvmsample.listener;
+
+/**
+ * Desc:
+ * Created by Hai (haigod7@gmail.com) on 2017/5/10 16:41.
+ */
+public interface OnItemClickListener {
+    void onItemClick(String item);
+}
