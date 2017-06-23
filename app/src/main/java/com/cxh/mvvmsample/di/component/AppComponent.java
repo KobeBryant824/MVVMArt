d3ee36874a368abf910363407e068e71666f6584
Hai <haigod7@gmail.com>
2017-06-23 15:03:18 +0800

    use java1.8 compile

---
+++ app/src/main/java/com/cxh/mvvmsample/di/component/AppComponent.java
@@ -0,0 +1,30 @@
+package com.cxh.mvvmsample.di.component;
+
+import android.content.Context;
+
+
+import com.cxh.mvvmsample.di.moduel.AppModule;
+import com.cxh.mvvmsample.di.qualifier.ContextLife;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+/**
+ * @author Hai (haigod7[at]gmail[dot]com)
+ *         2017/6/7 15:43
+ */
+@Singleton
+@Component(modules = AppModule.class)
+public interface AppComponent {
+
+    @ContextLife
+    Context getApplication();
+
+    Retrofit getRetrofit();
+
+    OkHttpClient getOkHttpClient();
+
+}
