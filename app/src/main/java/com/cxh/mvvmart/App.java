d6f63c331f66ab8a3e70eb2dea20d7fb22a11ec4
hai <haigod7@gmail.com>
2017-10-18 16:59:14 +0800

    update studio3.0

--- app/src/main/java/com/cxh/mvvmart/App.java
+++ app/src/main/java/com/cxh/mvvmart/App.java
@@ -50,6 +50,7 @@ public class App extends DaggerApplication implements Thread.UncaughtExceptionHa
 
         EventBus.builder()
                 .throwSubscriberException(Constant.BUILD)//只有在BUILD模式下，会抛出错误异常
+                .addIndex(new MyEventBusIndex())
                 .installDefaultEventBus();
 
         if (!Constant.BUILD) Thread.currentThread().setUncaughtExceptionHandler(this);
